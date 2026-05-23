4ef116d3ef66da8497748a3ee9cb56bf1a5c4c6a
gazifayaz <gazifayaz.16694@gmail.com>
2026-05-24 04:16:33 +0600

    Fix: add keepalive interval + uncaught error handlers to prevent Bun event loop drain

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
 import app from "./app.js";
 import { env } from "./config/env.js";
 
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught exception:", err);
+});
+
+process.on("unhandledRejection", (reason) => {
+  console.error("Unhandled rejection:", reason);
+});
+
 const server = app.listen(env.PORT, () => {
   console.log(`Server running on port ${env.PORT} [${env.NODE_ENV}]`);
 });
 
+const keepAlive = setInterval(() => {}, 60_000);
+
 process.on("SIGTERM", () => {
+  clearInterval(keepAlive);
   server.close(() => process.exit(0));
 });
 
 process.on("SIGINT", () => {
+  clearInterval(keepAlive);
   server.close(() => process.exit(0));
 });
